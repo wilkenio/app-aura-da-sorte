8fdc10949f4c861baaa11b208a2d476b952d1a2c
Wilkenio <wilkeniopereiraguitarra@gmail.com>
2026-03-22 11:59:53 -0300

    Fix import paths in tabs.routes.ts after reorganization

--- src/app/pages/tabs/tabs.routes.ts
+++ src/app/pages/tabs/tabs.routes.ts
@@ -9,12 +9,12 @@ export const routes: Routes = [
       {
         path: 'home',
         loadComponent: () =>
-          import('./home/home.page').then((m) => m.HomePage),
+          import('../home/home.page').then((m) => m.HomePage),
       },
       {
         path: 'stats',
         loadComponent: () =>
-          import('./stats/stats.page').then((m) => m.StatsPage),
+          import('../stats/stats.page').then((m) => m.StatsPage),
       },
       {
         path: '',
